79009f785bc277132d656bfb1a6306cdb6ed4205
S451 <paul257@ohs.stanford.edu>
2025-02-21 16:42:57 -0600

    streamlined auth process for websocket

--- websocketClient.py
+++ websocketClient.py
@@ -1,10 +1,14 @@
 import websocket
 import json
 import requests
+import uuid
 from bpro.pronto import *
 from bpro.systemcheck import *
 from bpro.readjson import *
-
+###
+###
+###
+###
 auth_path, chats_path, bubbles_path, loginTokenJSONPath, authTokenJSONPath, verificationCodeResponseJSONPath, settings_path, encryption_path, logs_path, settingsJSONPath, keysJSONPath, bubbleOverviewJSONPath, users_path = createappfolders()
 accesstoken = ""
 user_info = get_clientUserInfo(authTokenJSONPath)
@@ -21,20 +25,23 @@ def getLocalAccesstoken():
 getLocalAccesstoken()
 
 url = "wss://ws-mt1.pusher.com/app/f44139496d9b75f37d27?protocol=7&client=js&version=8.3.0&flash=false"
-
-# Define your access token
 api_base_url = "https://stanfordohs.pronto.io/"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {accesstoken}",
-}
+channel_name_id = get_org_id(authTokenJSONPath)
+###
+###
+###
+###
+###
 
-# Function to authenticate the subscription
-def authenticate_channel(socket_id, channel_name):
-    auth_url = f"{api_base_url}api/v1/pusher.auth"  # Replace with your backend URL
+#Function to authenticate the subscription
+def init_auth(socket_id, channel_name_id):
+    auth_url = f"{api_base_url}api/v1/pusher.auth"
+    privpush_uuid = uuid.uuid4()  
+
+    #payload for organization channel
     payload = {
         "socket_id": socket_id,
-        "channel_name": channel_name
+        "channel_name": f"private-organization.{channel_name_id}"
     }
     headers = {
         "Authorization": f"Bearer {accesstoken}",
@@ -43,49 +50,33 @@ def authenticate_channel(socket_id, channel_name):
 
     response = requests.post(auth_url, headers=headers, json=payload)
     response.raise_for_status()  # Check for HTTP errors
-    return response.json().get("auth")
+    print(f"Auth response: {response.json()}")
+    privorg_auth = response.json().get("auth")
+    print("Organization Connection Established.")
+    print(f"Organization Auth: {privorg_auth}")
 
-def on_message(ws, message):
-    print(f"Received message: {message}")
-    data = json.loads(message)
-    
-    if "event" in data and data["event"] == "pusher:connection_established":
-        inner_data = json.loads(data["data"])
-        socket_id = inner_data.get("socket_id")
-        if socket_id:
-            subscribe_to_channel(ws, socket_id)  # Pass ws and socket_id
-
-def on_error(ws, error):
-    print(f"Error: {error}")
-
-def on_close(ws):
-    print("Connection closed")
-
-def on_open(ws):
-    print("Connection opened")
-
-def subscribe_to_channel(ws, socket_id):
-    channel_name = "private-organization.2245"  # The channel to subscribe to
-    auth = authenticate_channel(socket_id, channel_name)
-
-    subscribe_message = {
-        "event": "pusher:subscribe",
-        "data": {
-            "channel": channel_name,
-            "auth": auth  # Add the auth information here
-        }
+    #payload for private channel
+    data = {
+        "socket_id": socket_id,
+        "channel_name": f"private-push.{userID}.{privpush_uuid}"
     }
+    response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()  # Check for HTTP errors
+    print(f"Auth response: {response.json()}")
+    privpush_auth = response.json().get("auth")
+    print("Push Connection Established.")
+    print(f"Push Auth: {privpush_auth}")
 
-    ws.send(json.dumps(subscribe_message))
+    #payload for user channel
+    data = {
+        "socket_id": socket_id,
+        "channel_name": f"private-user.{userID}"
+    }
+    response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()  # Check for HTTP errors
+    print(f"Auth response: {response.json()}")
+    privuser_auth = response.json().get("auth")
+    print("User Connection Established.")
+    print(f"User Auth: {privuser_auth}")
 
-# Create a WebSocketApp instance
-ws = websocket.WebSocketApp(url,
-                            on_message=on_message,
-                            on_error=on_error,
-                            on_close=on_close)
 
-# Add the on_open method
-ws.on_open = on_open
-
-# Run the WebSocket
-ws.run_forever()
